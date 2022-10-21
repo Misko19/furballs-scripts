37534872f0b129ed1874a40659733e54a2f2f16a
Mitch Miskoski <miskoski@gmail.com>
2022-10-21 15:47:11 -0500

    Auto stash before merge of "master" and "origin/master"

--- furball_equipment.py
+++ furball_equipment.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import requests
 import json
 import pandas as pd
@@ -37,3 +38,42 @@ json_data = json.loads(r.text)
 # print(df)
 
 print(json.dumps(json_data["data"]["furball"]["equipment"], indent=4))
+=======
+import requests
+import json
+import pandas as pd
+
+url = "https://furballs.com/api/graphql/"
+
+query = """ query {
+    furball(tokenId: "0x050809060900090408080700") {
+        id
+        name
+        level
+        zone
+        skillRollCost
+        skillUpgradesAvailable
+        equipment {
+            definition {
+                name
+            }
+            equippedBy {
+                id
+                socialName
+            }
+        }
+    }
+}"""
+
+r = requests.post(url, json={'query': query})
+# print(r.status_code)
+# print(r.text)
+
+json_data = json.loads(r.text)
+
+#df_data = json_data['data']['furball']
+#df = pd.DataFrame(df_data)
+# print(df)
+
+print(json.dumps(json_data["data"]["furball"]["equipment"], indent=4))
+>>>>>>> Stashed changes
